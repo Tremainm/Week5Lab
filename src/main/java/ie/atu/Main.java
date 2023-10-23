f61bf862c73d614bf73d40862438eae00f0998cd
Tremain <g00367973@atu.ie>
2023-10-23 09:56:00 +0100

    created an instance & added getters and setters

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,6 +2,8 @@ package ie.atu;
 
 public class Main {
     public static void main(String[] args) {
-
+        Person student1 = new Person("Paul","Lennon",18);
+        System.out.println("Details are: " + student1.getFirstName());
+        student1.displayInfo();
     }
 }
